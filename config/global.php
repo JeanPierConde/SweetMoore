c36552e9d456cea8c712b03ec6ce645cf60207c8
Jean Pier Conde Yaya <64507370+JeanPierConde@users.noreply.github.com>
2022-06-22 00:37:36 -0500

    Add files via upload

---
+++ config/global.php
@@ -0,0 +1,21 @@
+<?php 
+//ip de la pc servidor base de datos
+define("DB_HOST", "www.vpssistemas.com");
+
+// nombre de la base de datos
+define("DB_NAME", "vpssistemas_smoore");
+
+
+//nombre de usuario de base de datos
+define("DB_USERNAME", "vpssistemas_mglsweetmoore");
+
+//conraseña del usuario de base de datos
+define("DB_PASSWORD", "khL6yVT+j%Yy");
+
+//codificacion de caracteres
+define("DB_ENCODE", "utf8");
+
+//nombre del proyecto
+define("PRO_NOMBRE", "ITVentas");
+ 
+ ?>
